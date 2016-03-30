99881f34dae699d2804b9e46ddffec9fae38be85
jalauros <janne.lauros@gmail.com>
2016-03-30 10:27:43 +0300

    checkstyle corrections

--- idp-ssointerceptor-api-stepup/src/main/java/fi/csc/idp/stepup/api/StepUpEventIds.java
+++ idp-ssointerceptor-api-stepup/src/main/java/fi/csc/idp/stepup/api/StepUpEventIds.java
@@ -1,6 +1,6 @@
 /*
  * The MIT License
- * Copyright (c) 2015 CSC  IT Center for Science, http://www.csc.fi
+ * Copyright (c) 2015 CSC - IT Center for Science, http://www.csc.fi
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,6 +20,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package fi.csc.idp.stepup.api;
 
 import javax.annotation.Nonnull;
--- idp-ssointerceptor-api-stepup/src/main/java/fi/csc/idp/stepup/api/package-info.java
+++ idp-ssointerceptor-api-stepup/src/main/java/fi/csc/idp/stepup/api/package-info.java
@@ -21,25 +21,5 @@
  * THE SOFTWARE.
  */
 
+/** stepup api interfaces.*/
 package fi.csc.idp.stepup.api;
-
-/** Interface for performing challenge response verification. */
-public interface ChallengeVerifier {
-
-    /**
-     * Verifies that the response is acceptable for the challenge and target.
-     * 
-     * 
-     * @param challenge
-     *            Challenge generated for the target.
-     * @param response
-     *            Users response to the challenge
-     * @param target
-     *            input used for generating the challenge. Represents the user
-     *            responding to challenge.
-     * @return boolean true if the response was acceptable
-     */
-    boolean verify(final String challenge, final String response,
-            final String target);
-
-}
