913473011e9ef67c69219cad24633b03e7790342
jalauros <janne.lauros@gmail.com>
2016-05-20 13:17:44 +0300

    new shibsp context

--- idp-ssointerceptor-impl-stepup/src/main/java/fi/csc/idp/stepup/impl/CheckRequestedAuthenticationContext.java
+++ idp-ssointerceptor-impl-stepup/src/main/java/fi/csc/idp/stepup/impl/CheckRequestedAuthenticationContext.java
@@ -25,6 +25,7 @@ package fi.csc.idp.stepup.impl;
 
 import java.security.Principal;
 import java.util.Collection;
+import java.util.List;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -33,29 +34,21 @@ import javax.security.auth.Subject;
 import net.shibboleth.idp.attribute.IdPAttribute;
 import net.shibboleth.idp.authn.AbstractAuthenticationAction;
 import net.shibboleth.idp.authn.context.AuthenticationContext;
-import net.shibboleth.idp.saml.authn.principal.AuthnContextClassRefPrincipal;
-import net.shibboleth.idp.saml.authn.principal.AuthnContextDeclRefPrincipal;
 import net.shibboleth.utilities.java.support.annotation.constraint.NonnullElements;
 import net.shibboleth.utilities.java.support.component.ComponentSupport;
 import net.shibboleth.utilities.java.support.logic.Constraint;
 
-import org.opensaml.messaging.context.navigate.MessageLookup;
 import org.opensaml.profile.action.ActionSupport;
+import org.opensaml.profile.action.EventIds;
 import org.opensaml.profile.context.ProfileRequestContext;
-import org.opensaml.profile.context.navigate.InboundMessageContextLookup;
-import org.opensaml.saml.saml2.core.AuthnContextClassRef;
-import org.opensaml.saml.saml2.core.AuthnContextDeclRef;
-import org.opensaml.saml.saml2.core.AuthnRequest;
-import org.opensaml.saml.saml2.core.RequestedAuthnContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Function;
-import com.google.common.base.Functions;
 import com.google.common.base.Predicates;
 import com.google.common.collect.Collections2;
 
 import fi.csc.idp.stepup.api.StepUpEventIds;
+import fi.okm.mpass.shibboleth.authn.context.ShibbolethSpAuthenticationContext;
 
 /**
  * An action that checks if step up authentication is requested.
@@ -70,15 +63,7 @@ public class CheckRequestedAuthenticationContext extends
     @Nonnull
     private final Logger log = LoggerFactory
             .getLogger(CheckRequestedAuthenticationContext.class);
-
-    /** Lookup strategy function for obtaining {@link AuthnRequest}. */
-    @Nonnull
-    private Function<ProfileRequestContext, AuthnRequest> authnRequestLookupStrategy;
-
-    /** The request message to read from. */
-    @Nullable
-    private AuthnRequest authnRequest;
-
+    
     /** The attribute to match against. */
     @Nullable
     private IdPAttribute attribute;
@@ -86,60 +71,7 @@ public class CheckRequestedAuthenticationContext extends
     /** The authentication methods indicating step up. */
     @Nonnull
     private Subject stepupPrincipals;
-
-    /** Constructor. */
-    public CheckRequestedAuthenticationContext() {
-        log.trace("Entering");
-        authnRequestLookupStrategy = Functions.compose(new MessageLookup<>(
-                AuthnRequest.class), new InboundMessageContextLookup());
-        log.trace("Leaving");
-    }
-
-    /**
-     * Set the strategy used to locate the {@link AuthnRequest} to read from.
-     * 
-     * @param strategy
-     *            lookup strategy
-     */
-
-    public void setAuthnRequestLookupStrategy(
-            @Nonnull final Function<ProfileRequestContext, AuthnRequest> strategy) {
-        log.trace("Entering");
-        ComponentSupport
-                .ifInitializedThrowUnmodifiabledComponentException(this);
-        authnRequestLookupStrategy = Constraint.isNotNull(strategy,
-                "AuthnRequest lookup strategy cannot be null");
-        log.trace("Leaving");
-    }
-
-    /** {@inheritDoc} */
-    @SuppressWarnings("unchecked")
-    @Override
-    protected boolean doPreExecute(
-            @Nonnull final ProfileRequestContext profileRequestContext,
-            @Nonnull final AuthenticationContext authenticationContext) {
-
-        log.trace("Entering");
-        if (!super.doPreExecute(profileRequestContext, authenticationContext)) {
-            log.trace("Leaving");
-            return false;
-        }
-
-        authnRequest = authnRequestLookupStrategy.apply(profileRequestContext);
-        if (authnRequest == null) {
-            log.debug(
-                    "{} AuthnRequest message was not returned by lookup strategy",
-                    getLogPrefix());
-            
-            // TODO :Add StepUpEventIds.EXCEPTION to supported errors, map it
-            ActionSupport.buildEvent(profileRequestContext,
-                    StepUpEventIds.EXCEPTION);
-            log.trace("Leaving");
-            return false;
-        }
-        log.trace("Leaving");
-        return true;
-    }
+    
     
     /**
      * Sets the list of authentication methods requiring step up.
@@ -174,10 +106,21 @@ public class CheckRequestedAuthenticationContext extends
             @Nonnull final ProfileRequestContext profileRequestContext,
             @Nonnull final AuthenticationContext authenticationContext) {
         log.trace("Entering");
-        final RequestedAuthnContext requestedCtx = authnRequest
-                .getRequestedAuthnContext();
+     
+        final ShibbolethSpAuthenticationContext shibbolethContext = authenticationContext
+                .getSubcontext(ShibbolethSpAuthenticationContext.class);
+        if (shibbolethContext == null) {
+            log.debug("{} Could not get shib proxy context", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext,
+                    EventIds.INVALID_PROFILE_CTX);
+            log.trace("Leaving");
+            return;
+        }
         
-        if (requestedCtx == null || !stepupRequested(requestedCtx,stepupPrincipals)) {
+        if (shibbolethContext.getInitialRequestedContext() == null || 
+            shibbolethContext.getInitialRequestedContext().isEmpty() || 
+            !stepupRequested(shibbolethContext.getInitialRequestedContext(),stepupPrincipals)) {
+            
             log.debug(
                     "{} AuthnRequest did not contain a RequestedAuthnContext matching any StepUp",
                     getLogPrefix());
@@ -186,54 +129,25 @@ public class CheckRequestedAuthenticationContext extends
             log.trace("Leaving");
             return;
         }
+        
         ActionSupport.buildEvent(profileRequestContext,
                 StepUpEventIds.EVENTID_CONTINUE_STEPUP);
         log.trace("Leaving");
     }
     
     /**
-     * Method checks if the requested authentication method is listed as step up.
+     * Method checks if any of the requested authentication methods is listed as step up.
      * 
-     * @param requestedCtx  requested method
-     * @param stepups stepup methods
+     * @param requestedPrincipals  requested methods
+     * @param stepupPrincipals stepup methods
      * @return true if the requested method requires step up.
      */
-    private boolean stepupRequested(RequestedAuthnContext requestedCtx, Subject stepups){
+    private boolean stepupRequested(List<Principal> requestedPrincipals, Subject stepupPrincipals){
         log.trace("Entering");
-        for (AuthnContextClassRef authnContextClassRef : requestedCtx
-                .getAuthnContextClassRefs()) {
-            for (Principal matchingPrincipal : stepups
-                    .getPrincipals()) {
-                if (matchingPrincipal instanceof AuthnContextClassRefPrincipal
-                        && authnContextClassRef
-                                .getAuthnContextClassRef()
-                                .equals(((AuthnContextClassRefPrincipal) matchingPrincipal)
-                                        .getAuthnContextClassRef()
-                                        .getAuthnContextClassRef())) {
-                    log.debug("stepup requested {}",authnContextClassRef
-                                .getAuthnContextClassRef());
-                    log.trace("leaving");
-                    return true;
-                }
-
-            }
-        }
-        for (AuthnContextDeclRef authnContextDeclRef : requestedCtx
-                .getAuthnContextDeclRefs()) {
-            for (Principal matchingPrincipal : stepups
-                    .getPrincipals()) {
-                if (matchingPrincipal instanceof AuthnContextDeclRefPrincipal
-                        && authnContextDeclRef
-                                .getAuthnContextDeclRef()
-                                .equals(((AuthnContextDeclRefPrincipal) matchingPrincipal)
-                                        .getAuthnContextDeclRef()
-                                        .getAuthnContextDeclRef())) {
-                    log.debug("stepup requested {}",authnContextDeclRef
-                            .getAuthnContextDeclRef());
-                    log.trace("leaving");
-                    return true;
-                }
-
+        for (Principal requestedPrincipal : requestedPrincipals) {
+            if (stepupPrincipals.getPrincipals().contains(requestedPrincipal)){
+                log.trace("Leaving");
+                return true;
             }
         }
         log.trace("Leaving");
