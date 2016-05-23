92ced84b1c1c78f5d7f505bcc25be2b4a87139e5
jalauros <janne.lauros@gmail.com>
2016-05-23 09:08:21 +0300

    test cases added

--- idp-ssointerceptor-impl-stepup/src/test/java/fi/csc/idp/stepup/impl/TestCheckRequestedAuthenticationContext.java
+++ idp-ssointerceptor-impl-stepup/src/test/java/fi/csc/idp/stepup/impl/TestCheckRequestedAuthenticationContext.java
@@ -2,11 +2,12 @@ package fi.csc.idp.stepup.impl;
 
 import java.security.Principal;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
 
 import org.opensaml.profile.context.ProfileRequestContext;
-import org.springframework.util.Assert;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
 import org.testng.annotations.BeforeMethod;
@@ -19,6 +20,9 @@ import net.shibboleth.idp.authn.context.AuthenticationContext;
 import net.shibboleth.idp.profile.ActionTestingSupport;
 import net.shibboleth.idp.profile.RequestContextBuilder;
 import net.shibboleth.idp.profile.context.navigate.WebflowRequestContextProfileRequestContextLookup;
+import net.shibboleth.idp.saml.authn.principal.AuthnContextClassRefPrincipal;
+import net.shibboleth.idp.saml.authn.principal.AuthnContextDeclRefPrincipal;
+
 
 public class TestCheckRequestedAuthenticationContext {
 
@@ -28,22 +32,36 @@ public class TestCheckRequestedAuthenticationContext {
     @SuppressWarnings("rawtypes")
     protected ProfileRequestContext prc;
     
+    AuthnContextClassRefPrincipal class1=new AuthnContextClassRefPrincipal("test1");
+    AuthnContextClassRefPrincipal class2=new AuthnContextClassRefPrincipal("test2");
+    AuthnContextClassRefPrincipal class3=new AuthnContextClassRefPrincipal("test3");
+    AuthnContextClassRefPrincipal class4=new AuthnContextClassRefPrincipal("test4");
+    
+    AuthnContextDeclRefPrincipal decl1=new AuthnContextDeclRefPrincipal("test1");
+    AuthnContextDeclRefPrincipal decl2=new AuthnContextDeclRefPrincipal("test2");
+    AuthnContextDeclRefPrincipal decl3=new AuthnContextDeclRefPrincipal("test3");
+    AuthnContextDeclRefPrincipal decl4=new AuthnContextDeclRefPrincipal("test4");
+    
+    
+    
     @BeforeMethod public void setUp() throws Exception {        
         src = new RequestContextBuilder().buildRequestContext();
         prc = new WebflowRequestContextProfileRequestContextLookup().apply(src);
         action = new CheckRequestedAuthenticationContext();
-        action.initialize();
+        
     }
     
     /**  Test that action copes with no authentication context being present */
     @Test public void testUninitiailizedContext() throws ComponentInitializationException {
+        action.initialize();
         final Event event = action.execute(src);
         ActionTestingSupport.assertEvent(event, AuthnEventIds.INVALID_AUTHN_CTX);
     }
     
     /**  Test that action copes with no shibboleth context present */
     @Test public void testNoShibbolethContext() throws ComponentInitializationException {
-        AuthenticationContext ctx=(AuthenticationContext)prc.addSubcontext(new AuthenticationContext(), true);
+        prc.addSubcontext(new AuthenticationContext(), true);
+        action.initialize();
         final Event event=action.execute(src);
         ActionTestingSupport.assertEvent(event, StepUpEventIds.EXCEPTION);
     }
@@ -52,6 +70,7 @@ public class TestCheckRequestedAuthenticationContext {
     @Test public void testPartialShibbolethContext() throws ComponentInitializationException {
         AuthenticationContext ctx=(AuthenticationContext)prc.addSubcontext(new AuthenticationContext(), true);
         ctx.addSubcontext(new ShibbolethSpAuthenticationContext(),true);
+        action.initialize();
         final Event event=action.execute(src);
         ActionTestingSupport.assertEvent(event, StepUpEventIds.EVENTID_AUTHNCONTEXT_NOT_STEPUP);
     }
@@ -61,8 +80,50 @@ public class TestCheckRequestedAuthenticationContext {
         AuthenticationContext ctx=(AuthenticationContext)prc.addSubcontext(new AuthenticationContext(), true);
         ShibbolethSpAuthenticationContext shibspCtx=(ShibbolethSpAuthenticationContext)ctx.addSubcontext(new ShibbolethSpAuthenticationContext(),true);
         shibspCtx.setInitialRequestedContext(new ArrayList<Principal>());
+        action.initialize();
         final Event event=action.execute(src);
         ActionTestingSupport.assertEvent(event, StepUpEventIds.EVENTID_AUTHNCONTEXT_NOT_STEPUP);
+    }
+    
+    /**  Test that action distinguishes classrefs from declrefs */
+    @Test public void testNonMatchingPrincipals() throws ComponentInitializationException {
+        AuthenticationContext ctx=(AuthenticationContext)prc.addSubcontext(new AuthenticationContext(), true);
+        ShibbolethSpAuthenticationContext shibspCtx=(ShibbolethSpAuthenticationContext)ctx.addSubcontext(new ShibbolethSpAuthenticationContext(),true);
+        List<Principal> requested=new ArrayList<Principal>();
+        List<Principal> stepups=new ArrayList<Principal>();
+        requested.add(class1);
+        requested.add(class2);       
+        requested.add(decl3);
+        requested.add(decl4);
+        stepups.add(decl1);
+        stepups.add(decl2);
+        stepups.add(class3);
+        stepups.add(class4);
+        
+        shibspCtx.setInitialRequestedContext(requested);
+        action.setStepupMethods(stepups);
+        action.initialize();
+        final Event event=action.execute(src);
+        ActionTestingSupport.assertEvent(event, StepUpEventIds.EVENTID_AUTHNCONTEXT_NOT_STEPUP);
+    }
+    
+    /**  Test that action is able to find a match */
+    @Test public void testMatchingPrincipals() throws ComponentInitializationException {
+        AuthenticationContext ctx=(AuthenticationContext)prc.addSubcontext(new AuthenticationContext(), true);
+        ShibbolethSpAuthenticationContext shibspCtx=(ShibbolethSpAuthenticationContext)ctx.addSubcontext(new ShibbolethSpAuthenticationContext(),true);
+        AuthnContextClassRefPrincipal localClass4=new AuthnContextClassRefPrincipal("test4");
+        List<Principal> requested=new ArrayList<Principal>();
+        List<Principal> stepups=new ArrayList<Principal>();
+        requested.add(class1);
+        requested.add(class4);       
+        stepups.add(class2);
+        //should match requested class4
+        stepups.add(localClass4);
+        shibspCtx.setInitialRequestedContext(requested);
+        action.setStepupMethods(stepups);
+        action.initialize();
+        final Event event=action.execute(src);
+        ActionTestingSupport.assertEvent(event, StepUpEventIds.EVENTID_CONTINUE_STEPUP);
     }
     
 }
