9cb86c69c43f6435c479bbb5670470c7e89daf7a
jalauros <janne.lauros@gmail.com>
2016-05-31 14:39:00 +0300

    tests added

---
+++ idp-ssointerceptor-api-stepup/src/test/java/fi/csc/idp/stepup/api/TestStepUpContext.java
@@ -0,0 +1,21 @@
+package fi.csc.idp.stepup.api;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestStepUpContext {
+   
+    /**  Test that context i/o works as expected */
+    @Test public void testUninitiailizedContext() {
+        StepUpContext ctx=new StepUpContext();
+        Assert.assertNull(ctx.getChallenge());
+        Assert.assertNull(ctx.getTarget());
+        ctx.setChallenge("challenge");
+        ctx.setTarget("target");
+        Assert.assertEquals("challenge", ctx.getChallenge());
+        Assert.assertEquals("target", ctx.getTarget());
+        
+    }
+    
+
+}
