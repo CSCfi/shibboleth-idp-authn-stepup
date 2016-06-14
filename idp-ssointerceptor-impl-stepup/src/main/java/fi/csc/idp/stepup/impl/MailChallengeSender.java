1632dadeee6e89ff82c75095b378c01f47fc4e90
jalauros <janne.lauros@gmail.com>
2016-06-14 11:27:13 +0300

    res path added

--- idp-ssointerceptor-impl-stepup/src/main/java/fi/csc/idp/stepup/impl/MailChallengeSender.java
+++ idp-ssointerceptor-impl-stepup/src/main/java/fi/csc/idp/stepup/impl/MailChallengeSender.java
@@ -53,8 +53,7 @@ public class MailChallengeSender implements ChallengeSender {
 
     /** Class logger. */
     @Nonnull
-    private final Logger log  = LoggerFactory
-            .getLogger(MailChallengeSender.class);
+    private final Logger log = LoggerFactory.getLogger(MailChallengeSender.class);
 
     /** Sender address of email. */
     private String from;
@@ -72,111 +71,143 @@ public class MailChallengeSender implements ChallengeSender {
     private String host;
     /** port of the mail host. */
     private String port;
-    /** email template used to override the default.*/
+    /** email template used to override the default. */
     private String templateFileName;
-    
-    /** email  address. */
+    /** email template path used to override the default. */
+    private String templateFilePath;
+
+    /** email address. */
     private String to;
     /** body of the email. */
     private String body;
-    
-    
+
     /**
      * Sets the Sender field of email.
-     * @param fromField the value of the sender
+     * 
+     * @param fromField
+     *            the value of the sender
      */
     public void setFromField(String fromField) {
         log.trace("Entering");
-        log.debug("setting from to "+fromField);
+        log.debug("setting from to " + fromField);
         this.from = fromField;
         log.trace("Leaving");
     }
 
     /**
      * Sets the Subject field of email.
-     * @param subjectField the value of the sender
+     * 
+     * @param subjectField
+     *            the value of the sender
      */
     public void setSubjectField(String subjectField) {
         log.trace("Entering");
-        log.debug("setting subject to "+subjectField);
+        log.debug("setting subject to " + subjectField);
         this.subject = subjectField;
         log.trace("Leaving");
     }
-    
+
     /**
      * Sets the username of the email account.
-     * @param accountUserName is the username
+     * 
+     * @param accountUserName
+     *            is the username
      */
     public void setUserName(String accountUserName) {
         log.trace("Entering");
-        log.debug("setting account username to "+accountUserName);
+        log.debug("setting account username to " + accountUserName);
         this.userName = accountUserName;
         log.trace("Leaving");
     }
 
     /**
      * Sets the password of the email account.
-     * @param accountPassword is the Password
+     * 
+     * @param accountPassword
+     *            is the Password
      */
     public void setPassword(String accountPassword) {
         log.trace("Entering");
-        log.debug("setting account password to "+accountPassword);
+        log.debug("setting account password to " + accountPassword);
         this.password = accountPassword;
         log.trace("Leaving");
     }
-    
+
     /**
      * Sets the SMTP Auth property of the email server.
-     * @param serverSMTPAuth SMTP Auth property
+     * 
+     * @param serverSMTPAuth
+     *            SMTP Auth property
      */
     public void setSMTPAuth(String serverSMTPAuth) {
         log.trace("Entering");
-        log.debug("setting SMTP Auth to "+serverSMTPAuth);
-        this.smtpAuth=serverSMTPAuth;
+        log.debug("setting SMTP Auth to " + serverSMTPAuth);
+        this.smtpAuth = serverSMTPAuth;
         log.trace("Leaving");
     }
-    
+
     /**
      * Sets the SMTP TTLS Start property of the email server.
-     * @param serverSMTPTtls SMTP TTLS Start property
+     * 
+     * @param serverSMTPTtls
+     *            SMTP TTLS Start property
      */
     public void setSMTPTtls(String serverSMTPTtls) {
         log.trace("Entering");
-        log.debug("setting SMTP Start TLS to "+serverSMTPTtls);
-        this.smtpTtls=serverSMTPTtls;
+        log.debug("setting SMTP Start TLS to " + serverSMTPTtls);
+        this.smtpTtls = serverSMTPTtls;
         log.trace("Leaving");
     }
-    
+
     /**
      * Sets the email server host.
-     * @param hostName email server hostname
+     * 
+     * @param hostName
+     *            email server hostname
      */
     public void setHost(String hostName) {
         log.trace("Entering");
-        log.debug("setting host to "+hostName);
-        this.host=hostName;
+        log.debug("setting host to " + hostName);
+        this.host = hostName;
         log.trace("Leaving");
     }
-    
+
     /**
      * Sets the port of email server host.
-     * @param serverPort port of email server
+     * 
+     * @param serverPort
+     *            port of email server
      */
     public void setPort(String serverPort) {
         log.trace("Entering");
-        log.debug("setting port to "+serverPort);
-        this.port=serverPort;
+        log.debug("setting port to " + serverPort);
+        this.port = serverPort;
         log.trace("Leaving");
     }
-    
+
     /**
-     * Sets a path to a file replacing the default template.
-     * @param fileName template file replacing the default one
+     * Sets a template file name replacing the default template.
+     * 
+     * @param fileName
+     *            template file replacing the default one
      */
     public void setTemplateFile(String fileName) {
         log.trace("Entering");
-        log.debug("setting template filename to "+fileName);
+        log.debug("setting template filename to " + fileName);
         this.templateFileName = fileName;
+        log.trace("Leaving");
+    }
+
+    /**
+     * Sets a path to a file replacing the default template.
+     * 
+     * @param fileName
+     *            template file replacing the default one
+     */
+    public void setTemplatePath(String path) {
+        log.trace("Entering");
+        log.debug("setting path to template files" + path);
+        this.templateFilePath = path;
         log.trace("Leaving");
     }
 
@@ -188,65 +219,60 @@ public class MailChallengeSender implements ChallengeSender {
     private Template getVelocityTemplate() {
         log.trace("Entering");
         VelocityEngine velocityEngine = new VelocityEngine();
-        if (templateFileName != null && !templateFileName.isEmpty()) {
-            velocityEngine
-                    .setProperty(RuntimeConstants.RESOURCE_LOADER, "file");
+        if (templateFileName != null && !templateFileName.isEmpty() && templateFilePath != null
+                && !templateFilePath.isEmpty()) {
+            velocityEngine.setProperty(RuntimeConstants.RESOURCE_LOADER, "file");
+            velocityEngine.setProperty(RuntimeConstants.FILE_RESOURCE_LOADER_PATH, templateFilePath);
             velocityEngine.init();
             log.trace("Leaving");
             return velocityEngine.getTemplate(templateFileName);
         }
-        velocityEngine.setProperty(RuntimeConstants.RESOURCE_LOADER,
-                "classpath");
-        velocityEngine.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName()); 
+        velocityEngine.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+        velocityEngine.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
         velocityEngine.init();
         log.trace("Leaving");
-        return velocityEngine.getTemplate(File.separator + "emails"
-                + File.separator + "default.vm");
+        return velocityEngine.getTemplate(File.separator + "emails" + File.separator + "default.vm");
 
     }
-    
-    
-    //TODO: Make use of parameters and methods sane.. this is now
-    //a mess. What values are static and same for all, what is per
-    //execution..
-    
+
+    // TODO: Make use of parameters and methods sane.. this is now
+    // a mess. What values are static and same for all, what is per
+    // execution..
+
     @Override
     public void send(String challenge, String target) throws AddressException, MessagingException {
         log.trace("Entering");
-        log.debug("Sending challenge "+challenge+" to "+target);
-        to=target;
+        log.debug("Sending challenge " + challenge + " to " + target);
+        to = target;
         Template template = getVelocityTemplate();
         final VelocityContext velocityContext = new VelocityContext();
         velocityContext.put("otp", challenge);
         StringWriter writer = new StringWriter();
         template.merge(velocityContext, writer);
-        body=writer.toString();
+        body = writer.toString();
         Properties props = new Properties();
         props.put("mail.smtp.auth", smtpAuth);
         props.put("mail.smtp.starttls.enable", smtpTtls);
         props.put("mail.smtp.host", host);
         props.put("mail.smtp.port", port);
         Session session = null;
-        if (userName != null && password != null){
-            session = Session.getInstance(props,
-                    new javax.mail.Authenticator() {
-                        protected PasswordAuthentication getPasswordAuthentication() {
-                            return new PasswordAuthentication(
-                                    userName, password);
-                        }
-                    });
-        }else{
+        if (userName != null && password != null) {
+            session = Session.getInstance(props, new javax.mail.Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(userName, password);
+                }
+            });
+        } else {
             session = Session.getInstance(props);
         }
         Message message = new MimeMessage(session);
         try {
             message.setFrom(new InternetAddress(from));
-            message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse(to));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
             message.setSubject(subject);
             message.setText(body);
             Transport.send(message);
-            
+
         } catch (MessagingException e) {
             StringWriter sw = new StringWriter();
             e.printStackTrace(new PrintWriter(sw));
@@ -257,6 +283,5 @@ public class MailChallengeSender implements ChallengeSender {
         log.debug("Challenge sending triggered");
         log.trace("Leaving");
     }
-   
 
 }
