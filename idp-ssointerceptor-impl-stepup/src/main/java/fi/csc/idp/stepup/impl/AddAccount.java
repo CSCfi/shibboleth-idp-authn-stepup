1063c0aee1a74e5388c6b080a0ad7f2c89027a48
jalauros <janne.lauros@gmail.com>
2016-09-22 12:55:28 +0300

    first draft

---
+++ idp-ssointerceptor-impl-stepup/src/main/java/fi/csc/idp/stepup/impl/AddAccount.java
@@ -0,0 +1,107 @@
+/*
+ * The MIT License
+ * Copyright (c) 2015 CSC - IT Center for Science, http://www.csc.fi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package fi.csc.idp.stepup.impl;
+
+
+import javax.annotation.Nonnull;
+
+import net.shibboleth.idp.authn.AbstractAuthenticationAction;
+import net.shibboleth.idp.authn.context.AuthenticationContext;
+
+import org.opensaml.profile.action.ActionSupport;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import fi.csc.idp.stepup.api.StepUpEventIds;
+import fi.csc.idp.stepup.api.StepUpMethodContext;
+import fi.csc.idp.stepup.api.StepUpAccount;
+
+/**
+ * An action that generates a shared secret.The action selects attribute id and
+ * challenge generator on the basis of requested
+ * authentication context. Attribute value, if defined, is passed to challenge
+ * generator. Secret is stored to context.
+ * 
+ * 
+ */
+
+@SuppressWarnings("rawtypes")
+public class AddAccount extends AbstractAuthenticationAction {
+
+    /** Class logger. */
+    @Nonnull
+    private final Logger log = LoggerFactory.getLogger(AddAccount.class);
+
+
+    /** proxy StepUp Context. */
+    private StepUpMethodContext stepUpMethodContext;
+ 
+     /** {@inheritDoc} */
+    @SuppressWarnings("unchecked")
+    /** {@inheritDoc} */
+    @Override
+    protected boolean doPreExecute(@Nonnull final ProfileRequestContext profileRequestContext,
+            @Nonnull final AuthenticationContext authenticationContext) {
+
+        log.trace("Entering");
+        //TODO: FIX ERROR
+        stepUpMethodContext = authenticationContext.getSubcontext(StepUpMethodContext.class);
+        if (stepUpMethodContext == null) {
+            log.debug("{} Could not get shib proxy context", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, StepUpEventIds.EVENTID_MISSING_SHIBSPCONTEXT);
+            log.trace("Leaving");
+            return false;
+        }
+        return super.doPreExecute(profileRequestContext, authenticationContext);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext,
+            @Nonnull final AuthenticationContext authenticationContext) {
+        log.trace("Entering");
+        if (stepUpMethodContext.getStepUpMethod() == null){
+            log.debug("No default stepup method available", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, StepUpEventIds.EVENTID_INVALID_USER);
+            log.trace("Leaving");
+            return;
+        }
+        log.debug("Adding a stepup account of type "+stepUpMethodContext.getStepUpMethod().getName());
+        StepUpAccount account=stepUpMethodContext.getStepUpMethod().addAccount();
+        if (account == null){
+            log.debug("Could not create new stepup account for user", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, StepUpEventIds.EVENTID_INVALID_USER);
+            log.trace("Leaving");
+            return;
+        }
+        //We set the account as active
+        stepUpMethodContext.setStepUpAccount(account);
+        ActionSupport.buildEvent(profileRequestContext, StepUpEventIds.EVENTID_CONTINUE_STEPUP);
+        log.trace("Leaving");
+    }
+  
+
+}
