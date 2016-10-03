d2b92264d7e4ce8744e55213e861e93a07351826
jalauros <janne.lauros@gmail.com>
2016-10-03 15:31:45 +0300

    tests for AbstractStepUpAccount

---
+++ idp-ssointerceptor-impl-stepup/src/test/java/fi/csc/idp/stepup/impl/TestAbstractStepUpAccount.java
@@ -0,0 +1,136 @@
+package fi.csc.idp.stepup.impl;
+
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import fi.csc.idp.stepup.api.ChallengeGenerator;
+import fi.csc.idp.stepup.api.ChallengeVerifier;
+
+public class TestAbstractStepUpAccount {
+
+    private TestStepUpAccount testStepUpAccount;
+    private ChallengeGen challengeGen;
+    private ChallengeVer challengeVer;
+    
+    @BeforeMethod
+    public void setUp()  {
+        testStepUpAccount = new TestStepUpAccount();
+        challengeGen = new ChallengeGen();
+        challengeVer = new ChallengeVer();
+    }
+
+    /** test default behavior*/
+    @Test
+    public void testUnitialized() throws Exception  {
+        Assert.assertNull(testStepUpAccount.getChallenge());
+        Assert.assertNull(testStepUpAccount.getName());
+        Assert.assertEquals(testStepUpAccount.getId(),0);
+        Assert.assertNull(testStepUpAccount.getTarget());
+        Assert.assertTrue(testStepUpAccount.isEditable());
+        Assert.assertTrue(!testStepUpAccount.isEnabled());
+    }
+
+    private void setValues() {
+        testStepUpAccount.setTarget("target");
+        testStepUpAccount.setName("name");
+        testStepUpAccount.setId(1);
+        testStepUpAccount.setEnabled(true);
+        testStepUpAccount.setEditable(false);
+    }
+    private void setValues2() {
+        testStepUpAccount.setTarget("target2");
+        testStepUpAccount.setName("name2");
+        testStepUpAccount.setId(2);
+        testStepUpAccount.setEnabled(false);
+        testStepUpAccount.setEditable(true);
+    }
+    
+    /** test setters behavior*/
+    @Test
+    public void testSetters() throws Exception  {
+        setValues();
+        Assert.assertEquals(testStepUpAccount.getTarget(),"target");
+        Assert.assertEquals(testStepUpAccount.getName(),"name");
+        Assert.assertEquals(testStepUpAccount.getId(),1);
+        Assert.assertTrue(testStepUpAccount.isEnabled());
+        Assert.assertTrue(!testStepUpAccount.isEditable());
+        //editing disabled, should not have any impact
+        setValues2();
+        Assert.assertEquals(testStepUpAccount.getTarget(),"target");
+        Assert.assertEquals(testStepUpAccount.getName(),"name");
+        Assert.assertEquals(testStepUpAccount.getId(),1);
+        Assert.assertTrue(testStepUpAccount.isEnabled());
+        Assert.assertTrue(!testStepUpAccount.isEditable());
+        
+    }
+    
+    /** test challenge sender/generator behavior*/
+    @Test
+    public void testChallengeGenerator()   {
+        boolean exceptionOccurred=false;
+        try {
+            testStepUpAccount.sendChallenge();
+        } catch (Exception e) {
+            exceptionOccurred = true;
+        }
+        Assert.assertTrue(exceptionOccurred);
+        exceptionOccurred = false;
+        testStepUpAccount.setChallengeGenerator(challengeGen);
+        try {
+            testStepUpAccount.sendChallenge();
+        } catch (Exception e) {
+            exceptionOccurred = true;
+        }
+        Assert.assertTrue(!exceptionOccurred);
+        Assert.assertEquals("challengeGenerated", testStepUpAccount.getChallenge());
+    }
+    
+    /** test challenge verification behavior*/
+    @Test
+    public void testChallengeVerificator()   {
+        boolean exceptionOccurred=false;
+        try {
+            testStepUpAccount.verifyResponse("response");
+        } catch (Exception e) {
+            exceptionOccurred = true;
+        }
+        Assert.assertTrue(exceptionOccurred);
+        exceptionOccurred = false;
+        testStepUpAccount.setChallengeGenerator(challengeGen);
+        testStepUpAccount.setChallengeVerifier(challengeVer);
+        try {
+            testStepUpAccount.sendChallenge();
+            Assert.assertTrue(testStepUpAccount.verifyResponse("response"));
+        } catch (Exception e) {
+            exceptionOccurred = true;
+        }
+        Assert.assertTrue(!exceptionOccurred);
+    }
+    
+        
+    class TestStepUpAccount extends AbstractStepUpAccount {
+        
+        
+    }
+    
+    class ChallengeGen implements ChallengeGenerator {
+
+        @Override
+        public String generate(String target) throws Exception {
+            return "challengeGenerated";
+        }
+        
+    }
+    
+    class ChallengeVer implements ChallengeVerifier {
+
+        @Override
+        public boolean verify(String challenge, String response, String target) {
+            return response.equals("response")&&challenge.equals("challengeGenerated");
+        }
+        
+    }
+ 
+}
