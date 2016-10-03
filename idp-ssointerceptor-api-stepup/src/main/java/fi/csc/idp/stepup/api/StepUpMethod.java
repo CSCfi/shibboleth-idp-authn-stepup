dc47c068d36e5cf7fe534b4202213aaf74e41d71
jalauros <janne.lauros@gmail.com>
2016-10-03 08:30:32 +0300

    checkstyle fixes, reformatted

--- idp-ssointerceptor-api-stepup/src/main/java/fi/csc/idp/stepup/api/StepUpMethod.java
+++ idp-ssointerceptor-api-stepup/src/main/java/fi/csc/idp/stepup/api/StepUpMethod.java
@@ -1,30 +1,59 @@
+/*
+ * The MIT License
+ * Copyright (c) 2015 CSC - IT Center for Science, http://www.csc.fi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package fi.csc.idp.stepup.api;
 
 import java.util.List;
 
 import net.shibboleth.idp.attribute.context.AttributeContext;
 
+/** interface for managing Step Up Methods. */
 public interface StepUpMethod {
 
     /**
      * This is called before any other calls to initialize the Step Up Method
+     * and possibly existing accounts.
      * 
      * @param attributeContext
+     *            may be used by initialization.
+     * @return true if initialization was successful.
+     * @throws Exception
+     *             if something unexpected occurred
      */
 
-    public boolean Initialize(AttributeContext attributeContext) throws Exception;
+    public boolean initialize(AttributeContext attributeContext) throws Exception;
 
     /**
-     * Name of the stepup method.
+     * Name of the Step Up method.
      * 
-     * @return
+     * @return name of the method.
      */
     public String getName();
 
     /**
      * If accounts can be added or removed.
      * 
-     * @return true if accounts can be added..
+     * @return true if accounts can be added.
      */
     public boolean isEditable();
 
@@ -32,6 +61,8 @@ public interface StepUpMethod {
      * Existing accounts of the method.
      * 
      * @return list of accounts
+     * @throws Exception
+     *             if something unexpected occurred
      */
     public List<StepUpAccount> getAccounts() throws Exception;
 
@@ -39,6 +70,8 @@ public interface StepUpMethod {
      * Adds a new account.
      * 
      * @return new account.
+     * @throws Exception
+     *             if something unexpected occurred
      */
     public StepUpAccount addAccount() throws Exception;
 
