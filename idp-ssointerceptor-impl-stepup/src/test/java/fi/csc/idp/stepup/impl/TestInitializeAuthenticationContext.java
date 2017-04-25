544ad8df6b74387be0e6581898b75d01bd5dec3a
jalauros <janne.lauros@gmail.com>
2017-04-25 14:52:22 +0300

    tests for InitializeAuthenticationContext

---
+++ idp-ssointerceptor-impl-stepup/src/test/java/fi/csc/idp/stepup/impl/TestInitializeAuthenticationContext.java
@@ -0,0 +1,81 @@
+package fi.csc.idp.stepup.impl;
+
+import java.net.URI;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.minidev.json.JSONObject;
+import net.shibboleth.idp.authn.context.AuthenticationContext;
+import net.shibboleth.idp.profile.ActionTestingSupport;
+import net.shibboleth.idp.profile.RequestContextBuilder;
+import net.shibboleth.idp.profile.context.navigate.WebflowRequestContextProfileRequestContextLookup;
+
+import org.opensaml.profile.action.EventIds;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.springframework.webflow.execution.Event;
+import org.springframework.webflow.execution.RequestContext;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jwt.JWT;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import com.nimbusds.oauth2.sdk.ResponseType;
+import com.nimbusds.oauth2.sdk.Scope;
+import com.nimbusds.oauth2.sdk.id.ClientID;
+import com.nimbusds.oauth2.sdk.id.State;
+import com.nimbusds.openid.connect.sdk.AuthenticationRequest;
+import com.nimbusds.openid.connect.sdk.Nonce;
+
+import fi.csc.idp.stepup.api.OidcProcessingEventIds;
+import fi.csc.idp.stepup.api.OidcStepUpContext;
+
+public class TestInitializeAuthenticationContext {
+
+    private InitializeAuthenticationContext action;
+
+    protected RequestContext src;
+    @SuppressWarnings("rawtypes")
+    protected ProfileRequestContext prc;
+    protected OidcStepUpContext oidcCtx;
+    protected Map<String, String> claimToAttributeMap;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        AuthenticationRequest req = AuthenticationRequest
+                .parse("max_age=0&response_type=code&client_id=s6BhdRkqt3&login_hint=foo&redirect_uri=https%3A%2F%2Fclient.example.org%2Fcb&scope=openid%20profile&state=af0ifjsldkj&nonce=n-0S6_WzA2Mj");
+        src = new RequestContextBuilder().buildRequestContext();
+        prc = new WebflowRequestContextProfileRequestContextLookup().apply(src);
+        oidcCtx = new OidcStepUpContext();
+        prc.addSubcontext(oidcCtx);
+        action = new InitializeAuthenticationContext();
+        oidcCtx.setRequest(req);
+        action.initialize();
+
+    }
+
+    /**
+     * Test that action is able to build authentication context
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testSuccess() throws Exception {
+        final Event event = action.execute(src);
+        Assert.assertNull(event);
+        AuthenticationContext ctx = prc.getSubcontext(AuthenticationContext.class, false);
+        Assert.assertNotNull(ctx);
+        Assert.assertTrue(ctx.isForceAuthn());
+
+    }
+
+}
