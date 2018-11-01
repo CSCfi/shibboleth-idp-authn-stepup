068cfb1218e6ee9d3c4043a6bce230cea2d71042
jalauros <janne.lauros@gmail.com>
2018-11-01 14:54:03 +0200

    simplified, extends now abstract

--- idp-ssointerceptor-impl-stepup/src/test/java/fi/csc/idp/stepup/impl/MockAccount.java
+++ idp-ssointerceptor-impl-stepup/src/test/java/fi/csc/idp/stepup/impl/MockAccount.java
@@ -24,49 +24,12 @@
 package fi.csc.idp.stepup.impl;
 
 import fi.csc.idp.stepup.api.LimitReachedException;
-import fi.csc.idp.stepup.api.StepUpAccount;
-import fi.csc.idp.stepup.event.api.AccountRestrictorAction;
 
-public class MockAccount implements StepUpAccount {
+public class MockAccount extends AbstractStepUpAccount {
 
     public String correctResponse = "response_success";
-    long id = 0;
-    String name;
-    
+
     public boolean noRetries;
-
-    @Override
-    public long getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean isEditable() {
-        return false;
-    }
-
-    @Override
-    public void setEditable(boolean isEditable) {
-    }
-
-    @Override
-    public void setEnabled(boolean isEnabled) {
-    }
 
     @Override
     public boolean isEnabled() {
@@ -74,39 +37,21 @@ public class MockAccount implements StepUpAccount {
     }
 
     @Override
-    public void sendChallenge() throws Exception {
-    }
-
-    @Override
     public boolean verifyResponse(String response) throws Exception {
         if (response == null && correctResponse == null) {
             return true;
         }
-        boolean resp=correctResponse.equals(response);
-        if (resp == false && noRetries){
+        boolean resp = correctResponse.equals(response);
+        if (resp == false && noRetries) {
             throw new LimitReachedException("failure limit reached");
         }
         return resp;
     }
 
     @Override
-    public void setTarget(String target) {
-    }
-
-    @Override
-    public String getTarget() {
-        return null;
-    }
-
-    @Override
-    public boolean isVerified() {
+    protected void doSendChallenge() throws Exception {
         // TODO Auto-generated method stub
-        return false;
-    }
-    
-    @Override
-    public void setAccountRestrictor(AccountRestrictorAction restrictor){
-        // TODO Auto-generated method stub            
+
     }
 
 }
